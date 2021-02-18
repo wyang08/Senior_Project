812a947e95716eeefb57c6b8be530579cb7db556
wyang08 <wyang08@nyit.edu>
2021-02-17 22:39:41 -0500

    Basic setup

--- app/src/main/java/com/programming_concept/senior_project/MainActivity.java
+++ app/src/main/java/com/programming_concept/senior_project/MainActivity.java
@@ -1,14 +1,32 @@
 package com.programming_concept.senior_project;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DrawerLayout drawer;
+    Button button01;
+    Button button02;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
     }
 }
