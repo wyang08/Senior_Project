17df821c9ca74b209ab1724230577796968f2332
ralibert727 <32683269+ralibert727@users.noreply.github.com>
2021-04-08 00:26:27 -0400

    Questionnaire

--- app/src/main/java/com/programming_concept/senior_project/QuestionnaireFagment.java
+++ app/src/main/java/com/programming_concept/senior_project/QuestionnaireFagment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,8 +13,17 @@ import androidx.fragment.app.Fragment;
 
 public class QuestionnaireFagment extends Fragment {
     @Nullable
+
+    RadioButton response1, response2;
+    TextView header1, Question;
+
+    private Questions aQuestions = new Questions();
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragmnet_questionnair, container, false);
+
+
+
     }
 }
