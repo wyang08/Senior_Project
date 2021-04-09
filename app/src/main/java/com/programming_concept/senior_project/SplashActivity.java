2ee90ca2c41014a7675a98dc3825fd98d238c8ef
wyang08 <wyang08@nyit.edu>
2021-04-09 11:31:15 -0400

    condition - splash.acitivty

--- app/src/main/java/com/programming_concept/senior_project/SplashActivity.java
+++ app/src/main/java/com/programming_concept/senior_project/SplashActivity.java
@@ -1,8 +1,10 @@
 package com.programming_concept.senior_project;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.view.WindowManager;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AlphaAnimation;
@@ -11,9 +13,15 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.internal.GoogleApiAvailabilityCache;
+
 public class SplashActivity extends AppCompatActivity {
 
     private static int SPLASH_SCREEN_TIMEOUT = 5000;
+//    GoogleSignInClient mGoogleClient;
+    GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -31,12 +39,20 @@ public class SplashActivity extends AppCompatActivity {
 
         image.setAnimation(fadeOut);
 
+
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+
+                if (mGoogleApiClient != null) {
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Intent intent = new Intent(SplashActivity.this, Login.class);
+                    startActivity(intent);
+                }
+
 
             }
         }, SPLASH_SCREEN_TIMEOUT);
